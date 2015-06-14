81bb1bb5f6408b1276cbb5dc1555ea4c42000530
Luay <lkawasme@gmail.com>
2015-06-13 19:16:34 -0700

    update db with dummy data

--- StartupCollection.js
+++ StartupCollection.js
@@ -1,44 +1,80 @@
 Meteor.startup(function() {
    if(Meteor.isServer && Parents.find().fetch().length === 0) {
     var initialTestParent = {
-      username: "TestParent",
-      password: "pass",
-      email: "wilfred.wee@outlook.com"
+      username: "joannesmith",
+      password: "Password1",
+      email: "joannesmith.smith@outlook.com"
     };
 
     var parentId = Parents.insert(initialTestParent);
 
     var initialTestChild = {
-      firstName: "Test",
-      lastName: "Child",
-      dob: new Date(),
-      gender: "Male",
+      firstName: "Sara",
+      lastName: "Smith",
+      dob: new Date("20051005"),
+      gender: "Female",
       parentId: parentId,
-      bedTime: 22
+      bedTime: 1200
     };
 
     var childId = Children.insert(initialTestChild);
 
-    var initialProblem = {
-      code: "1",
-      name: "Awakening"
+    var child2 = {
+      firstName: "Sam",
+      lastName: "Smith",
+      dob: new Date("20050402"),
+      gender: "Male",
+      parentId: parentId,
+      bedTime: 1200
     };
 
-    var initialProblemId = Problems.insert(initialProblem);
+    var childId = Children.insert(initialTestChild);
 
-    var initialTrackable = {
-      notifyAt: 200,
+    var bedtimeProblem = {
+      code: "sleep_bedtime",
+      name: "What time did your child go to bed?"
+    };
+
+    var bedtimeProblemId = Problems.insert(bedtimeProblem);
+
+    var sleeptimeProblem = {
+      code: "sleep_sleeptime",
+      name: "What time did your child fall asleep?"
+    };
+
+    var sleeptimeProblemId = Problems.insert(sleeptimeProblem);
+
+    var sleep_beforemood = {
+      code: "sleep_beforemood",
+      name: "How would you describe the mood of your child before going to bed?"
+    };
+
+    var sleep_beforemoodProblemId = Problems.insert(sleep_beforemood);
+
+    var bedtimeTrackable = {
+      notifyAt: 1260,
       promptInterval: 1440,
       childId: childId,
-      problemId: initialProblemId,
+      problemId: bedtimeProblemId,
       isProblemForChild: true
     };
 
-    var initialTrackableId = Trackables.insert(initialTrackable);
+    var bedtimeTrackableId = Trackables.insert(bedtimeTrackable);
+
+    var sleep_beforemoodTrackable = {
+      notifyAt: 1260,
+      promptInterval: 1440,
+      childId: childId,
+      problemId: sleep_beforemoodProblemId,
+      isProblemForChild: false
+    };
+
+    var sleep_beforemoodTrackableId = Trackables.insert(sleep_beforemoodTrackable);
+
 
     var initialEvent = {
       timeStamp: new Date(),
-      trackableId: initialTrackableId
+      trackableId: bedtimeTrackableId
     };
 
     var initialEventId = Events.insert(initialEvent);
